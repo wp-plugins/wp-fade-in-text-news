17c9ae2afad2c73636dfd7d8c3c0c17476d23d27
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-19 14:48:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-fade-in-text-news/trunk@465088 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ setting.js
@@ -0,0 +1,106 @@
+/*
+##########################################################################################################
+###### Project   : Fade in text news  																######
+###### File Name : content-management.php                   										######
+###### Author    : Gopi.R (http://www.gopiplus.com/work/)                        					######
+###### Link      : http://www.gopiplus.com/work/2011/04/22/wordpress-plugin-wp-fadein-text-news/    ######
+##########################################################################################################
+*/
+
+/**
+ *     WP fade in text news
+ *     Copyright (C) 2011  www.gopiplus.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function FadeIn_submit()
+{
+	if(document.FadeIn_form.FadeIn_text.value=="")
+	{
+		alert("Please enter the message.")
+		document.FadeIn_form.FadeIn_text.focus();
+		return false;
+	}
+	else if(document.FadeIn_form.FadeIn_link.value=="")
+	{
+		alert("Please enter the link.")
+		document.FadeIn_form.FadeIn_link.focus();
+		return false;
+	}
+	else if(document.FadeIn_form.FadeIn_status.value=="")
+	{
+		alert("Please select the display status.")
+		document.FadeIn_form.FadeIn_status.focus();
+		return false;
+	}
+	else if(document.FadeIn_form.FadeIn_group.value=="")
+	{
+		alert("Please enter the group name. this field is used to group the announcement.")
+		document.FadeIn_form.FadeIn_group.focus();
+		return false;
+	}
+	else if(document.FadeIn_form.FadeIn_order.value=="")
+	{
+		alert("Please enter the display order, only number.")
+		document.FadeIn_form.FadeIn_order.focus();
+		return false;
+	}
+	else if(isNaN(document.FadeIn_form.FadeIn_order.value))
+	{
+		alert("Please enter the display order, only number.")
+		document.FadeIn_form.FadeIn_order.focus();
+		return false;
+	}
+	_FadeIn_escapeVal(document.FadeIn_form.FadeIn_text,'<br>');
+}
+
+function _FadeIn_delete(id)
+{
+	if(confirm("Do you want to delete this record?"))
+	{
+		document.FadeIn_Display.action="options-general.php?page=wp-fade-in-text-news/content-management.php&AC=DEL&DID="+id;
+		document.FadeIn_Display.submit();
+	}
+}	
+
+function _FadeIn_redirect()
+{
+	window.location = "options-general.php?page=wp-fade-in-text-news/content-management.php";
+}
+
+function _FadeIn_escapeVal(textarea,replaceWith)
+{
+textarea.value = escape(textarea.value) //encode textarea strings carriage returns
+for(i=0; i<textarea.value.length; i++)
+{
+	//loop through string, replacing carriage return encoding with HTML break tag
+	if(textarea.value.indexOf("%0D%0A") > -1)
+	{
+		//Windows encodes returns as \r\n hex
+		textarea.value=textarea.value.replace("%0D%0A",replaceWith)
+	}
+	else if(textarea.value.indexOf("%0A") > -1)
+	{
+		//Unix encodes returns as \n hex
+		textarea.value=textarea.value.replace("%0A",replaceWith)
+	}
+	else if(textarea.value.indexOf("%0D") > -1)
+	{
+		//Macintosh encodes returns as \r hex
+		textarea.value=textarea.value.replace("%0D",replaceWith)
+	}
+}
+textarea.value=unescape(textarea.value) //unescape all other encoded characters
+}
