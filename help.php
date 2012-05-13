4467933f46178878b636e05ad8d1cfad1386f229
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-13 10:19:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-fade-in-text-news/trunk@543666 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- help.php
+++
@@ -1,5 +0,0 @@
-<h2>Plugin Management!</h2>
-Click "Go to - Text Management" button to add/update/delete the messages. <br />
-Click "Go to - Fadein Setting" button to update scroller setting.
-<h2>How to use!</h2>
-<a target="_blank" href='http://www.gopiplus.com/work/2011/04/22/wordpress-plugin-wp-fadein-text-news/'>We have three option check this link</a>
